13acf18ca9c8f36ef8c99dd750c235d8db6d9a92
Tharindu H <tharindudeshan500@gmail.com>
2022-10-27 11:30:09 -0400

    Add app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dbPsswd = process.env.MONGODB_PSSWD;
+const dbUrl = `mongodb+srv://tharindu:${dbPsswd}@cluster0.pycye8m.mongodb.net/?retryWrites=true&w=majority`;
+const dbModels = require('./models/schema');
+const pageRank = require('./lab5');
+
+let rank = pageRank.getPageRank().then(x => {
+  rank = x;
+});
+
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(console.log("Connected to mongodb"))
+  .catch(err => console.log(err));
+
+// Automatically parse JSON data
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
+});
