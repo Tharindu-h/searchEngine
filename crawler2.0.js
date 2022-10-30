cce85f374874f5f45ad6339741aff96340a1f31c
Tharindu H <tharindudeshan500@gmail.com>
2022-10-30 16:44:56 -0400

    Add crawler to crawl manpages

---
+++ crawler2.0.js
@@ -0,0 +1,91 @@
+const Crawler = require("crawler");
+const fs = require("fs");
+const mongoose = require('mongoose');
+const dbPsswd = process.env.MONGODB_PSSWD;
+const dbUrl = `mongodb+srv://tharindu:${dbPsswd}@cluster0.pycye8m.mongodb.net/?retryWrites=true&w=majority`;
+const dbModels = require('./models/schema');
+
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(console.log("Connected to mongodb"))
+  .catch(err => console.log(err));
+
+let seenUrls = {};
+let urlCount = 0;
+let crawledPages = [];
+
+function getUrl(link, currPath){
+  if(link.includes('https://man7.org/linux/man-pages/')){
+    return link;
+  }
+  else if(link.slice(0,3) == '../'){
+    return `https://man7.org/linux/man-pages/${link.replace("../", "")}`;
+  }
+  else{
+    return null;
+  }
+}
+
+const c = new Crawler({
+  maxConnections : 10
+});
+
+function crawlUrl(url){
+  if (seenUrls[url] || urlCount >= 1) return;
+  seenUrls[url] = true;
+  urlCount++;
+  console.log(`Crawling: ${url}`);
+  c.queue({
+    uri: url,
+    callback: function(err, res, done){
+      if (err) console.log(err);
+      else{
+        let $ = res.$;
+        let links = $("a");
+        let title = ($("title").text()).replace(' - Linux manual page','');
+        let contents = $("pre").text();
+        let page = new dbModels.ManPage({
+          "url": url,
+          "title": title,
+          "outgoingLinks": [],
+          "contents": contents,
+          "numOutLinks": 0
+        });
+        $(links).each(function(i, link){
+          if($(link).attr('href')){
+            let currURL = getUrl($(link).attr('href'), url);
+            if (currURL != null){
+              crawlUrl(currURL);
+              page.outgoingLinks.push(currURL);
+              page.numOutLinks++;
+            }
+          }
+        });
+        crawledPages.push(page);
+      }
+      done();
+    }
+  });
+}
+
+crawlUrl('https://man7.org/linux/man-pages/man1/bash.1.html');
+c.on("drain", function(){
+  //saveAllPages();
+  console.log(crawledPages[0].url);
+  console.log(crawledPages[0].title);
+  console.log(crawledPages[0].links);
+});
+
+function saveAllPages(){
+  crawledPages.forEach(async (item) =>{
+    try{
+      await item.save(function(err){
+        if(err){console.log(err);}
+      });
+    }
+    catch (e){
+      console.log(e);
+    }
+  });
+}
